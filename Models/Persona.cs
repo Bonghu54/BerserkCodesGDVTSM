35b01ad7f1dc604d175757d707ca7e0cb478affc
Bonghu54 <elwea1969ka@gmail.com>
2023-11-03 20:51:50 -0400

    Agregacion de Clases solo con atributos

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -1,7 +1,16 @@
-﻿namespace BerserkCodesGDVTSM.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BerserkCodesGDVTSM.Models
 {
     public class Persona
     {
         public int Id_Persona { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Ci { get; set; }
+        [DataType(DataType.Date)]
+        public DateOnly Fecha_Nacimiento { get; set; }
+        public string Direccion { get; set; }
+
     }
 }
